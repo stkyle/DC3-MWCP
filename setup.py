b03742f24e9581b06641aaaec7d672f166b94763
stkyle <stkyle@users.noreply.github.com>
2016-02-18 11:25:33 -0500

    pep8

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@
 import os
 import re
 import sys
-from codecs import open
+from codecs import open as codecs_open
 from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.test import test as TestCommand
 
 
 class PyTest(TestCommand):
+    """Use Pytest for setup option `test`.
+    """
     user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
 
     def initialize_options(self):
@@ -33,42 +35,37 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
-packages = [
-    'mwcp',
-]
+REQUIRES = []
 
-requires = []
-
-test_requirements = ['pytest>=2.8.0', ]
+REQUIRES_TEST = ['pytest>=2.8.0', ]
 
 # Retrieve version from package variable __version__
-with open('mwcp/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+with codecs_open('mwcp/__init__.py', 'r') as fd:
+    VERSION = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
-if not version:
+if not VERSION:
     raise RuntimeError('Cannot find version information')
 
-with open('README.md', 'r', 'utf-8') as f:
-    readme = f.read()
-with open('HISTORY.md', 'r', 'utf-8') as f:
-    history = f.read()
+with codecs_open('README.md', 'r', 'utf-8') as f:
+    README = f.read()
+with codecs_open('HISTORY.md', 'r', 'utf-8') as f:
+    HISTORY = f.read()
 
 
 setup(
-    name = 'mwcp',
-    version = version,
-    author = "DC3",
-    description = 'A framework malware configuration parsers.',
-    long_description = readme + '\n\n' + history,
-    license = "MIT",
-    keywords = "malware",
-    url = "http://github.com/Defense-Cyber-Crime-Center/DC3-MWCP/",
-    packages = find_packages(),
-    package_data = {
-        '': ['*.txt', '*.json', 'resources/*.txt', 'resources/*.json']
-        },
-    scripts = [ "mwcp-tool.py", "mwcp-client.py", "mwcp-server.py", "mwcp-test.py" ],
-    extras_require = {'recommended': ['pefile', 'yara', 'pyCrypto', 'pydasm']
-    },
+    name='mwcp',
+    version=VERSION,
+    author="DC3",
+    description='A framework malware configuration parsers.',
+    long_description=README + '\n\n' + HISTORY,
+    license="MIT",
+    keywords="malware",
+    url="http://github.com/Defense-Cyber-Crime-Center/DC3-MWCP/",
+    packages=find_packages(),
+    package_data={'': ['*.txt', '*.json',
+                       'resources/*.txt', 'resources/*.json']},
+    scripts=["mwcp-tool.py", "mwcp-client.py",
+             "mwcp-server.py", "mwcp-test.py"],
+    extras_require={'recommended': ['pefile', 'yara', 'pyCrypto', 'pydasm']},
 )
